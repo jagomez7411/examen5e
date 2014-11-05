8a04c9a1b693e7933b2cb96cec25e912ebcacb74
jAntonioGomez7411 <aredcbtis16@hotmail.com>
2014-11-05 09:09:39 -0600

    4

--- jquery-mobile/acciones.js
+++ jquery-mobile/acciones.js
@@ -1,2 +1,30 @@
 // JavaScript Document
 
+$(document).ready(function(e){
+	document.addEventListener("deviceready",function(){
+		
+		$('#izquierda').swipeleft(function(){
+			navigator.notification.alert("Deslizó a la izquierda",function(){"Practica4","Aceptar"});
+			});//barrer izquierda
+			
+			
+			$('#derecha').swiperight(function(){
+				navigator.notification.confirm("¿Que quieres hacer?",function(opt){
+			switch(opt)
+			{
+			
+			case 1:
+			navigator.notification.beep(1);
+			break;
+			
+			case 2:
+			navigator.notification.vibrate(1000);
+			break;
+			}//switch opt
+				},"Practica4","Beep,Vibrar,Cancelar");//confirm
+				
+			});//barrer derecha
+			
+			
+	},false);//ready device
+});//document
